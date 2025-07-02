141864286bc75bdc6424a95a82a86695a5a29ecf
DarthKenar - 🏠 <federico.vega2222@gmail.com>
2025-07-01 23:03:41 -0300

    practice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+// use std::io::{stdout};
+// use ferris_says::say;
+
 fn main() {
-    println!("Hello, world!");
+    println!("hi");
+    // let writer = BufWriter::new(stdout().lock());
+    // let cosa: Result<(), std::io::Error> = say("input", 5,stdout());
 }

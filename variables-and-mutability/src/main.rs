6560395ab94ab1c917d8617708bac47ef8037419
DarthKenar - 🏠 <federico.vega2222@gmail.com>
2025-09-21 16:47:02 -0300

    test

--- variables-and-mutability/src/main.rs
+++ variables-and-mutability/src/main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+fn main() {
+    let apples = 50;
+    let oranges = 14 + 6;
+    let fruits = apples + oranges;
+    println!(
+        "We have {1} apples & {0} oranges. {fruits} in total",
+        apples, oranges
+    );
+}
